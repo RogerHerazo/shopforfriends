0ac96eac97be4ca10e0e43bba6141f143e62b64b
RogerHerazo <roger.andres031@gmail.com>
2020-06-07 13:03:36 -0500

    Complete Navigation of Pages

--- lib/Pages/checkout.dart
+++ lib/Pages/checkout.dart
@@ -1,5 +1,7 @@
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:shopforfriends/Models/product.dart';
+import 'package:shopforfriends/Pages/end.dart';
 class Checkout extends StatefulWidget {
   const Checkout({Key key, @required this.productlist, this.friendproductlist}) : super(key: key);
 
@@ -16,66 +18,103 @@ class _CheckoutState extends State<Checkout> {
       appBar: AppBar(
         title: Text("Checkout"),
       ),
-      body: Center(
+      body: ChangeNotifierProvider<SingleModel>(
+        create: (context) => SingleModel(chkstate : "Close"),
+        child: _buildUI(widget.productlist, widget.friendproductlist)     
+      ), 
+    );
+  }
+}
+
+Widget _buildUI(List<Product> productlist, List<Product> friendproductlist){
+  return Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           mainAxisSize: MainAxisSize.min,
           children: <Widget> [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget> [
-                Text("My Shopping List"),
-              ],
-            ),
-            Flexible(
-              child: Card(
-              margin: const EdgeInsets.all(5.0),
-              child: //Text("hi"),
-              ListView.builder(
-              shrinkWrap: false,
-              itemCount: widget.productlist.length,
-              itemBuilder: (context,index){
-              return Card(
-                margin: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                      Text(widget.productlist[index].name+"\nPrecio: \$"+widget.productlist[index].price.toString()),
+            Expanded(
+              child: Container(
+                child: Card(
+                  child: Column(
+                    children: <Widget> [
+                      Text("My Shopping List"),
+                      Expanded(
+                        child: ListView.builder(
+                          shrinkWrap: false,
+                          itemCount: productlist.length,
+                          itemBuilder: (context,index){
+                          return Card(
+                            margin: const EdgeInsets.all(10.0),
+                              child: Center(
+                                child: Text(productlist[index].name+"\nPrecio: \$"+productlist[index].price.toString()),
+                              )
+                            );
+                        })
+                      )
                     ]
                   )
-                );
-            })
-            )
+                )
+              )
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget> [
-                Text("My Friend Shopping List")
-              ],
-            ),
-            Flexible(
-              child: Card(
-              margin: const EdgeInsets.all(5.0),
-              child: //Text("hi"),
-              ListView.builder(
-              shrinkWrap: false,
-              itemCount: widget.productlist.length,
-              itemBuilder: (context,index){
-              return Card(
-                margin: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                      Text(widget.productlist[index].name+"\nPrecio: \$"+widget.productlist[index].price.toString()),
+            Expanded(
+              child: Container(
+                child: Card(
+                  child: Column(
+                    children: <Widget> [
+                      Text("My Friend Shop List"),
+                      Expanded(
+                        child: ListView.builder(
+                          shrinkWrap: false,
+                          itemCount: productlist.length,
+                          itemBuilder: (context,index){
+                          return Card(
+                            margin: const EdgeInsets.all(10.0),
+                              child: Center(
+                                child: Text(productlist[index].name+"\nPrecio: \$"+productlist[index].price.toString()),
+                              )
+                            );
+                        })
+                      )
                     ]
                   )
+                )
+              )
+            ),
+            Consumer<SingleModel>(
+              builder: (context, singleModel, child){
+                return Container(
+                  child: RaisedButton(
+                onPressed: () {
+                    
+                    if(singleModel.chkstate == "Close"){
+                      print("Chekout...");
+                      singleModel.changeValue("Finalize");
+                    }else{
+                      print("Finalize...");
+                      _pushPage(context, End());
+                    }
+                    
+                  },
+                  child: Text(singleModel.chkstate),
+                  ),
                 );
-            })
-            )
+              },
             )
           ],
         ),
-      ) 
-    );
+      );
+}
+
+class SingleModel extends ChangeNotifier {
+  String chkstate;
+  SingleModel({this.chkstate});
+
+  void changeValue(String chk) {
+    chkstate = chk;
+    notifyListeners(); 
   }
+}
+
+void _pushPage(BuildContext context, Widget page){
+  Navigator.pushAndRemoveUntil(context, MaterialPageRoute<void>(builder: (_) => page), (route) => false);
 }
---
+++ lib/Pages/end.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:shopforfriends/Pages/home.dart';
+import 'package:shopforfriends/Pages/login.dart';
+class End extends StatefulWidget {
+  @override
+  _EndState createState() => _EndState();
+}
+
+class _EndState extends State<End> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Checkout"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget> [
+                Card(
+                  child: Column(
+                    children: <Widget> [
+                      Text("Thanks For Your Buy")
+                    ]
+                  )
+                ),
+                RaisedButton(
+                onPressed: () {
+                    print("Chekout...");
+                    _pushPage(context, Login());
+                  },
+                  child: Text("LogOut"),
+                ),
+                RaisedButton(
+                onPressed: () {
+                    print("Chekout...");
+                    _pushPage(context, Home());
+                  },
+                  child: Text("Shop Again"),
+                ),
+          ],
+        ),
+      )
+    );
+  }
+}
+
+void _pushPage(BuildContext context, Widget page){
+  Navigator.pushAndRemoveUntil(context, MaterialPageRoute<void>(builder: (_) => page), (route) => false);
+}
